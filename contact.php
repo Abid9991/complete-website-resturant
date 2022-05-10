1a193dc9f822812510d87a542614ede315e5b226
Abid9991 <lashariabid07@gamil.com>
2022-05-10 11:47:29 +0500

    project

---
+++ contact.php
@@ -0,0 +1,58 @@
+<?php include "header.php"; ?>	
+	<!-- Start All Pages -->
+		
+		<img src="images/cn.png" width="100%">
+	<!-- End All Pages -->
+	
+	<!-- Start Contact -->
+	<div>
+	<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14420.817815066654!2d68.3807435!3d25.3644634!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x3b24a6c23a1f1718!2sBoulevard%20Mall%20Hyderabad!5e0!3m2!1sen!2s!4v1650000195971!5m2!1sen!2s" width="900" height="800" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+	</div>
+	<div class="contact-box">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="heading-title text-center">
+						<h2>Contact</h2>
+						<p>Contact Information are Listed here Thanks for Visit Our Site</p>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-12">
+					<form id="contactForm">
+						<div class="row">
+							<div class="col-md-12">
+								<div class="form-group">
+									<h1 style="font-family: cambria; color: red; text-align: center;">GodBites Restaurent</h1>
+									<div class="help-block with-errors"></div>
+								</div>                                 
+							</div>
+							<div class="col-md-12">
+								<div class="form-group">
+									<center><span style="color: tomato; font-size: 1.3em; text-align: center; "><b>Email Us :</b>alisher@gmail.com</span></center>
+									<div class="help-block with-errors"></div>
+								</div> 
+							</div>
+							<div class="col-md-12">
+								<div class="form-group">
+									<center><span style="color: tomato; font-size: 1.3em; text-align: center; "><b>Mobile  :</b> +923480332379</span></center>
+									<div class="help-block with-errors"></div>
+								</div> 
+							</div>
+							<div class="col-md-12">
+								<div class="form-group"> 
+									<center><span style="color: tomato; font-size: 1.3em; text-align: center; "><b>Location  :</b> Near Moti Masjid Hyderabad</span></center>
+									<div class="help-block with-errors"></div>
+								</div>
+								
+							</div>
+						</div>            
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- End Contact -->
+<?php include "footer.php"; ?>
---
+++ footer.php
@@ -0,0 +1,109 @@
+	
+	<!-- Start Contact info -->
+	<div class="contact-imfo-box">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4 arrow-right">
+					<i class="fa fa-volume-control-phone"></i>
+					<div class="overflow-hidden">
+						<h4>Phone</h4>
+						<p class="lead">
+							+93480332379
+						</p>
+					</div>
+				</div>
+				<div class="col-md-4 arrow-right">
+					<i class="fa fa-envelope"></i>
+					<div class="overflow-hidden">
+						<h4>Email</h4>
+						<p class="lead">
+							alisher@gmail.com
+						</p>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<i class="fa fa-map-marker"></i>
+					<div class="overflow-hidden">
+						<h4>Location</h4>
+						<p class="lead">
+							Near Moti Masjid
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- End Contact info -->
+	
+	<!-- Start Footer -->
+	<footer class="footer-area bg-f">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-3 col-md-6">
+					<h3>About Us</h3>
+					<p>There are a lot of ways you can go with your tagline if you’re not trying to tie in a specific cuisine. You could choose to emphasize your great value, a fun or romantic atmosphere, or the high quality of your product.</p>
+				</div>
+				<div class="col-lg-3 col-md-6">
+					<h3>UseFull Links</h3>
+					<div class="subscribe_form">
+						
+						<a href="index.php">Home</a><br>
+						<a href="contact.php">Contact Us</a><br>
+						<a href="menu.php">Food Menu</a><br>
+						<a href="gallery.php">Gallery</a><br>
+						
+						
+					</div>
+					<ul class="list-inline f-social">
+						<li class="list-inline-item"><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+						<li class="list-inline-item"><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+						<li class="list-inline-item"><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+						<li class="list-inline-item"><a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
+						<li class="list-inline-item"><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+					</ul>
+				</div>
+				<div class="col-lg-3 col-md-6">
+					<h3>Contact information</h3>
+					<p class="lead">G.O.R COLONY Hyderabad Near Moti Masjid</p>
+					<p class="lead"><a href="#">+923480332379</a></p>
+					<p><a href="#"> alisher@gmail.com</a></p>
+				</div>
+				<div class="col-lg-3 col-md-6">
+					<h3>Opening hours</h3>
+					<p><span class="text-color">Monday: </span> 5pm - 12PM</p>
+					<p><span class="text-color">Tue-Wed :</span> 5pm - 12PM</p>
+					<p><span class="text-color">Thu-Fri :</span> 5pm - 12PM</p>
+					<p><span class="text-color">Sat-Sun :</span> 5pm - 12PM</p>
+				</div>
+			</div>
+		</div>
+		
+		<div class="copyright">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12">
+						<p class="company-name"> &copy; 2021 <a href="#">
+					</div>
+				</div>
+			</div>
+		</div>
+		
+	</footer>
+	<!-- End Footer -->
+	
+	<a href="#" id="back-to-top" title="Back to top" style="display: none;"><i class="fa fa-paper-plane-o" aria-hidden="true"></i></a>
+
+	<!-- ALL JS FILES -->
+	<script src="js/jquery-3.2.1.min.js"></script>
+	<script src="js/popper.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+    <!-- ALL PLUGINS -->
+	<script src="js/jquery.superslides.min.js"></script>
+	<script src="js/images-loded.min.js"></script>
+	<script src="js/isotope.min.js"></script>
+	<script src="js/baguetteBox.min.js"></script>
+	<script src="js/form-validator.min.js"></script>
+    <script src="js/contact-form-script.js"></script>
+    <script src="js/custom.js"></script>
+</body>
+</html>
